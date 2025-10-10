7ed9e7531f64839d1dc6b4dac91ee8c76da838c2
Egor Volkov <eivolkov@edu.hse.ru>
2025-10-10 03:15:00 +0300

    Realization for constructors were added

--- include/linalg_realization.hpp
+++ include/linalg_realization.hpp
@@ -1,65 +1,65 @@
 #include <iostream>
+#include <initializer_list>
 
-namespace linalg
-{
-    std::size_t;
+
+namespace linalg{
+
+using std::size_t;
+using std::initializer_list;
+
+class Matrix {
+private:
+    //sources
+    double* m_ptr = nullptr; //отметим его нулевым указателем ради избежания возможных ошибок(UB) в дальнейшем(например при вызове дистрактора)
+    size_t m_rows, m_columns, m_capacity;
+
+public:
+    //metods 
+    bool matr_empty() const {return m_rows == 0 || m_columns == 0;}
+    size_t matr_rows() const {return m_rows;}
+    size_t matr_columns() const {return m_columns;}
+    size_t matr_capacity() const {return m_capacity;}
+    size_t matr_size() const {return m_columns * m_rows;}
+    const double* matr_begin() const {return m_ptr;} //указатель на 1 элемент матрицы  
+    const double* matr_end () const {return m_ptr ? m_ptr + matr_size(): m_ptr;} //если m_ptr не nullptr, то вернёт m_ptr + size(). а если m_ptr =nullptr, тогда вернет nullptr; ?-тернарный оператор(сокращенный вар. if ... else)
     
-    class Matrix {
-    private:
-        //sources
-        double* m_ptr = nullptr; //отметим его нулевым указателем ради избежания возможных ошибок(UB) в дальнейшем(например при вызове дистрактора)
-        size_t m_rows;
-        size_t m_columns;
-        size_t m_capacity;
-
-    public:
-        //metods 
-        bool matr_empty() const {return m_rows == 0 || m_columns == 0;}
-        size_t matr_rows() const {return m_rows;}
-        size_t matr_columns() const {return m_columns;}
-        size_t matr_capacity() const {return m_capacity;}
-        size_t matr_size() const {return m_columns * m_rows;}
-        const double* matr_begin() const {return m_ptr;} //указатель на 1 элемент матрицы  
-        const double* matr_end () const {return m_ptr ? m_ptr + matr_size(): m_ptr;} //если m_ptr не nullptr, то вернёт m_ptr + size(). а если m_ptr =nullptr, тогда вернет nullptr; ?-тернарный оператор(сокращенный вар. if ... else)
-        
-        //matrix modification methods
-        void reshape(size_t rows, size_t cols);
-        void reserve(size_t n);
-        void clear() noexcept; // очистка матрицы не дает исключений и ошибок, вместимость прежняя 
-        void shrink_to_fit(size_t size);
-        void swap(Matrix& other) noexcept; //смена элементов тоже не дает исключений и ошибок
-        void swap(Matrix& first, Matrix& second) noexcept {first.swap(second);} //глобальная функция обмена между двумя матрицами 
-
-        //matrix constructors 
-        Matrix() noexcept {}; //default constr
-        Matrix(const Matrix& other); //copying constr, no noexcept потому, что не можем гарантировать отсутствие исключений(например при выделении памяти)
-        Matrix(Matrix&& other) noexcept; //moving constr
-        Matrix(int rows, int columns); //добавил конструктор с параметрами
-        ~Matrix() {delete[] m_ptr;} //destructor(~ обозначает деструктор)
-
-        //copying and assignment opperators(используем operator= для перегрузки)
-        Matrix& operator=(const Matrix& other); // оператор копирующего присваивания 
-        Matrix& operator=(Matrix&& other) noexcept; // оператор перемещающего присваивания(не const так как забирает русурсы у объекта при копировании)
-
-
-        //matrix Operators
-        //subtraction  
-        Matrix operator -() const;
-        Matrix operator -(const Matrix& other) const;
-        Matrix operator -=(const Matrix& other);
-        //smmation
-        Matrix operator +() const;
-        Matrix operator +(const Matrix& other) const;
-        Matrix operator +=(const Matrix& other);
-        //checking equalities and inequalities
-        bool operator ==(const Matrix&) const;
-        bool operator !=(const Matrix&) const;
-        //multiplication
-        Matrix operator *(const Matrix& other); //матрица на матрицу 
-        friend Matrix operator *(double x, const Matrix& other); //число на матрицу  
-        Matrix operator *(double x) const;
-        Matrix operator *=(const Matrix& other); //умножение и присваивание результата операции
-        Matrix operator *=(double x); //поэлементарное умножение и присваивание результата 
+    //matrix modification methods
+    void reshape(size_t rows, size_t cols);
+    void reserve(size_t n);
+    void clear() noexcept; // очистка матрицы не дает исключений и ошибок, вместимость прежняя 
+    void shrink_to_fit(size_t size);
+    void swap(Matrix& other) noexcept; //смена элементов тоже не дает исключений и ошибок
+    void swap(Matrix& first, Matrix& second) noexcept {first.swap(second);} //глобальная функция обмена между двумя матрицами 
+    //matrix constructors 
+    Matrix() noexcept : m_rows(0), m_columns(0), m_capacity(0), m_ptr(nullptr){} //default constr, m_capacity = 0 because of 0 size of the matrix 
+    Matrix(const Matrix& other); //copying constr, no noexcept потому, что не можем гарантировать отсутствие исключений(например при выделении памяти)
+    Matrix(Matrix&& other) noexcept; //moving constr
+    Matrix(int rows, int columns); //добавил конструктор с параметрами
+    //place for unified initial constructors
     
-    };
+    
+    ~Matrix() {delete[] m_ptr;} //destructor(~ обозначает деструктор)
+    //copying and assignment opperators(используем operator= для перегрузки)
+    Matrix& operator=(const Matrix& other); // оператор копирующего присваивания 
+    Matrix& operator=(Matrix&& other) noexcept; // оператор перемещающего присваивания(не const так как забирает русурсы у объекта при копировании)
+    //matrix Operators
+    //subtraction  
+    Matrix operator -() const;
+    Matrix operator -(const Matrix& other) const;
+    Matrix operator -=(const Matrix& other);
+    //smmation
+    Matrix operator +() const;
+    Matrix operator +(const Matrix& other) const;
+    Matrix operator +=(const Matrix& other);
+    //checking equalities and inequalities
+    bool operator ==(const Matrix&) const;
+    bool operator !=(const Matrix&) const;
+    //multiplication
+    Matrix operator *(const Matrix& other); //матрица на матрицу 
+    friend Matrix operator *(double x, const Matrix& other); //число на матрицу  
+    Matrix operator *(double x) const;
+    Matrix operator *=(const Matrix& other); //умножение и присваивание результата операции
+    Matrix operator *=(double x); //поэлементарное умножение и присваивание результата 
+    
+};
 }
--- source/linalg_realization.cpp
+++ source/linalg_realization.cpp
@@ -1,6 +1,76 @@
-#include <iostream>
+#include <stdio.h>
+#include "linalg_realization.hpp"
 
-int main(){
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace linalg;
+using std::size_t;
+
+
+//constructors
+Matrix::Matrix(const Matrix& other) : m_rows(other.m_rows), m_columns(other.m_columns){ //copying constructor 
+    m_capacity = m_rows * m_columns;
+    m_ptr = new double[m_capacity];
+    
+    for(int i = 0; i < m_capacity; ++i)
+        m_ptr[i] = other.m_ptr[i];
 }
+
+
+Matrix::Matrix(Matrix&& other) noexcept : m_ptr(other.m_ptr), m_rows(other.m_rows), m_columns(other.m_columns), m_capacity(other.m_capacity){ //moving constructor
+    other.m_ptr = nullptr;
+    other.m_rows = other.m_columns = other.m_capacity = 0; //we have moved all values from target matrix to other matrix and imitializing old matrix with nulls
+} 
+
+
+Matrix::Matrix(int rows, int columns) : Matrix(){ //constructor with parameters using default constructor 
+    //we are using default constructor btw
+    m_rows = rows;
+    m_columns = columns;
+    m_capacity = m_rows*m_columns;
+    m_ptr = new double[m_capacity]{0}; //allocating memmory with nulls 
+}
+
+//place for unified initial constructor
+
+
+//operators
+Matrix& Matrix::operator=(const Matrix& other){ //copy assignment operator
+    if(this == &other)//checking if matrix is the same to avoid copying same matrices
+        return *this;
+    
+    size_t needed_capacity = other.m_rows * other.m_columns;
+    
+    if(m_capacity < needed_capacity){//comparing matrices capacity to avoid useless memmory allocation
+        delete[] m_ptr; //deliting old results of previous memmory allocations(in case they were complited)
+        m_capacity = needed_capacity;
+        m_ptr = new double[m_capacity];
+    }
+
+    for(int i = 0; i < m_capacity; ++i){
+        m_ptr[i] = other.m_ptr[i];
+    }
+    
+    m_rows = other.m_rows;
+    m_columns = other.m_columns;
+    
+    return *this; 
+} 
+
+
+Matrix& Matrix::operator=(Matrix&& other) noexcept{
+    if(this == &other)//checking if matrix is the same to avoid copying same matrices
+        return *this;
+    
+    delete[] m_ptr;
+    m_ptr = other.m_ptr; //copying new array as lvalue 
+    m_rows = other.m_rows; 
+    m_columns = other.m_columns;
+    m_capacity = other.m_capacity;
+    
+    other.m_ptr = nullptr;
+    other.m_rows = 0;
+    other.m_columns = 0;
+    other.m_capacity = 0;
+    return *this;
+}
+
+
